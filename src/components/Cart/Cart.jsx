0e8e0034810c62002d0ed8f4836e6bbfc2e4b715
pronoyNath <pronoynath890@gmail.com>
2023-09-15 01:34:01 +0600

    selected cart condition added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,10 +2,10 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 
-const Cart = ({selectedCourses}) => {
+const Cart = ({selectedCourses,totalCredit,remainingCredit,totalPrices}) => {
     return (
         <div className='bg-base-100 shadow-2xl p-5 rounded-xl space-y-3'>
-            <h2 className='font-semibold text-[#2F80ED]'>Credit Hour Remaining </h2>
+            <h2 className='font-semibold text-[#2F80ED]'>Credit Hour Remaining {remainingCredit>0 ? remainingCredit : 20} hr</h2>
             <hr />
             <h3 className='text-2xl font-bold pb-2'>Course Name:</h3>
             <ul className='list-decimal pl-5'>
@@ -16,9 +16,9 @@ const Cart = ({selectedCourses}) => {
                 }
             </ul>
             <hr />
-            <h2 className='font-semibold'>Total Credit Hour : 13</h2>
+            <h2 className='font-semibold'>Total Credit Hour : {totalCredit>0 ? totalCredit : '00'}</h2>
             <hr />
-            <h2 className='font-semibold'>Total Price : 48000 USD</h2>
+            <h2 className='font-semibold'>Total Price : {totalPrices>0 ? totalPrices : '00'} USD</h2>
         </div>
     );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,6 +5,9 @@ import Cart from '../Cart/Cart';
 const Home = () => {
     const [courses, setCourses] = useState([]);
     const [selectedCourses, setSelectedCourses] = useState([]);
+    const [totalCredit, setTotalCredit] = useState([]);
+    const [remainingCredit, setRemainingCredit] = useState([]);
+    const [totalPrices, setTotalPrices] = useState([]);
 
 
     useEffect(() => {
@@ -13,41 +16,74 @@ const Home = () => {
             .then(data => setCourses(data))
     }, []);
 
-    const handleSelectCourse =  (course)=>{
-        const newCourses = [...selectedCourses,course];
-        setSelectedCourses(newCourses);
+    const handleSelectCourse = (course) => {
+        const isExists = selectedCourses.find(selected => selected.id === course.id);
+        if (isExists) {
+            return alert("Already Selected.");
+        }
+        else {
+            let totalCredit = course.credit_hr;
+            selectedCourses.forEach(course =>
+                totalCredit = totalCredit + course.credit_hr)
+
+                if(totalCredit>20){
+                    return alert("all credit finished.")
+                }
+                else{
+                   
+                    const remainingCredit = 20 - totalCredit;
+                    setRemainingCredit(remainingCredit);
+                      
+                    console.log(remainingCredit);
+                    let totalPrice = course.price;
+                    selectedCourses.forEach(course=>
+                        totalPrice = totalPrice + course.price
+                    )
+                    const fixedTotal = totalPrice.toFixed(2);
+                    setTotalPrices(fixedTotal);
+
+                    const newCourses = [...selectedCourses, course];
+                    setSelectedCourses(newCourses);
+
+                    setTotalCredit(totalCredit);
+                        
+                }
+        }
     }
 
     return (
         <div className='flex gap-5'>
             <div className='flex-1 grid grid-cols-3 gap-5'>
-            {
-                courses.map(course => (
-                    <div key={course.id} className="card w-72 bg-base-100 shadow-2xl">
-                        <figure className="px-5 pt-5">
-                            <img src={course.img} alt="Shoes" className="rounded-xl" />
-                        </figure>
-                        <div className="card-body">
-                            <h2 className="card-title font-bold text-sm">{course.title}</h2>
-                            <p className='text-sm text-start'>{course.description}</p>
-                            <div className='flex gap-6 space-y-1 items-center'>
-                                <p className='font-semibold'><span><i className="fa-solid fa-dollar-sign"></i></span> Price: {course.price}</p>
-                                <p className='font-semibold'><span><i className="fa-solid fa-book-open"></i></span> Credit: {course.credit_hr}hr</p>
-                            </div>
-                            <div className="card-actions">
-                                <button onClick={()=>handleSelectCourse(course)} className="btn btn-primary w-full">Select</button>
+                {
+                    courses.map(course => (
+                        <div key={course.id} className="card w-72 bg-base-100 shadow-2xl">
+                            <figure className="px-5 pt-5">
+                                <img src={course.img} alt="Shoes" className="rounded-xl" />
+                            </figure>
+                            <div className="card-body">
+                                <h2 className="card-title font-bold text-sm">{course.title}</h2>
+                                <p className='text-sm text-start'>{course.description}</p>
+                                <div className='flex gap-6 space-y-1 items-center'>
+                                    <p className='font-semibold'><span><i className="fa-solid fa-dollar-sign"></i></span> Price: {course.price}</p>
+                                    <p className='font-semibold'><span><i className="fa-solid fa-book-open"></i></span> Credit: {course.credit_hr}hr</p>
+                                </div>
+                                <div className="card-actions">
+                                    <button onClick={() => handleSelectCourse(course)} className="btn btn-primary w-full">Select</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))
-            }
-        </div>
-        {/* selected courses */}
-        <div className='w-72'>
-            <Cart
-            selectedCourses={selectedCourses}
-            ></Cart>
-        </div>
+                    ))
+                }
+            </div>
+            {/* selected courses */}
+            <div className='w-72'>
+                <Cart
+                    selectedCourses={selectedCourses}
+                    totalCredit={totalCredit}
+                    remainingCredit={remainingCredit}
+                    totalPrices={totalPrices}
+                ></Cart>
+            </div>
 
         </div>
     );
